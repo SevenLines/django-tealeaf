900faa0821058b9d2617e04c4f750e71a24891d1
Mick <mmailm@mail.ru>
2014-03-27 02:14:02 +0900

    Добавил яндекс метрику

--- toc/tests.py
+++ toc/tests.py
@@ -4,13 +4,12 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from cms.api import create_page
+from django.contrib.auth.models import User
 
 from django.test import TestCase
+from toc.cms_plugins import TocPlugin
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TocPluginTests(TestCase):
+    pass
