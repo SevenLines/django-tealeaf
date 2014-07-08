07fb0106ee6ca6ecbd60aa3675d0ccf3bc265b5e
m <mmailm@mail.ru>
2014-07-08 19:16:10 +0900

    Перешел на compass, добавил в fabfile.py процедуру компиляции css

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,16 @@
-from fabric.api import run, env, cd, roles, prefix
+from fabric.api import run, env, cd, prefix
+from fabric.operations import local
 
 env.hosts = ['phosphorus.locum.ru']
 env.user = 'hosting_mmailm'
 env.activate = 'source ~/env-tealeaf/bin/activate'
+
+compile_css_find = "find -name config.rb -execdir compass {} \;"
+
+
+def compile_css(parameter='compile'):
+    local(compile_css_find.format(parameter))
+
 
 def deploy():
     app_dir = "~/projects/django-tealeaf"
@@ -10,5 +18,7 @@ def deploy():
         with prefix(env.activate):
             run("git pull")
             run("python manage.py migrate")
+            run(compile_css_find.format("clean"))
+            run(compile_css_find.format("compile"))
             run("python manage.py collectstatic --noinput")
             run("touch django.wsgi")
