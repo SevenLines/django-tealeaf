0710f256a707d131fe563a02fae9897362d292a4
m <mmailm@mail.ru>
2014-07-08 20:26:20 +0900

    Убрал из static sass и coffee. Написал соответствующие скрипты компиляции.

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,10 @@ env.activate = 'source ~/env-tealeaf/bin/activate'
 compile_css_find = "find -name config.rb -execdir compass {} \;"
 
 
+def generate_js(minify='-m'):
+    local("find -name *.coffee -execdir coffeebar -m -o ../static/js/ %s {} \;" % minify)
+
+
 def compile_css(parameter='compile'):
     local(compile_css_find.format(parameter))
 
@@ -18,7 +22,5 @@ def deploy():
         with prefix(env.activate):
             run("git pull")
             run("python manage.py migrate")
-            run(compile_css_find.format("clean"))
-            run(compile_css_find.format("compile"))
             run("python manage.py collectstatic --noinput")
             run("touch django.wsgi")
