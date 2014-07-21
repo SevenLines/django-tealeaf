a62f2b50edcb761d6900bac2aa35822ccd2c4d4d
m <mmailm@mail.ru>
2014-07-21 17:06:20 +0900

    фиксы

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,8 @@ urlpatterns = i18n_patterns('',
 
 if settings.DEBUG:
     urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls')),
+    ) + urlpatterns
 
