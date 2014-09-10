39d7332bbd4eeb33d4730c59fa4209b008038c26
m <mmailm@mail.ru>
2014-09-11 02:26:32 +0900

    упаковал базовые скрипты для таблицы оценок

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,28 @@ module.exports = function (grunt) {
                     'templates/static/js/interface.js',
                 ],
                 dest: 'templates/static/main.js'
+            },
+            marks_script: {
+                src: [
+                    'templates/static/lib/jquery.pickmeup.min.js',
+                    'templates/static/lib/jquery.pickmeup.twitter-bootstrap.min.js',
+                    'templates/static/lib/jquery.qtip.min.js',
+                    'templates/static/lib/knockout.js',
+                    'templates/static/lib/color.js',
+//                    'templates/static/js/ko.modal_confirm.js',
+//                    'students/static/js/students/ko.marks.js'
+                ],
+                dest: 'students/static/js/students/marks.js'
             }
         },
         uglify: {
             main_script: {
                 src: 'templates/static/main.js',
                 dest: 'templates/static/main.min.js'
+            },
+            marks_script: {
+                src: 'students/static/js/students/marks.js',
+                dest: 'students/static/js/students/marks.min.js'
             }
         }
     });
@@ -25,4 +41,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('main_script', ['concat:main_script', 'uglify:main_script']);
+    grunt.registerTask('marks_script', ['concat:marks_script', 'uglify:marks_script']);
 };
