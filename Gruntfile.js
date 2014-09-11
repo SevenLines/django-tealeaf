ca3ceacade15a84caf50afc476b68665e5ff616c
m <mmailm@mail.ru>
2014-09-11 21:09:06 +0900

    убрал bootstrap из bower использую скачанный вручную, сделал нормальную склейку скриптов

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,12 @@ module.exports = function (grunt) {
         concat: {
             main_style: {
                 src: [
-                    'templates/static/bower_components/bootstrap/dist/css/bootstrap.min.css',
+                    'templates/static/lib/bootstrap/bootstrap.min.css',
                     'templates/static/bower_components/qtip2/qtip.min.css',
                     'templates/static/bower_components/pickmeup/pickmeup.min.css',
+                    'templates/static/css/style.css'
                 ],
-                dest: 'templates/static/main.css'
+                dest: 'templates/static/css/main.css'
             },
             main_script: {
                 src: [
@@ -16,7 +17,7 @@ module.exports = function (grunt) {
                     'templates/static/bower_components/jquery.cookie/jquery.cookie.js',
                     'templates/static/bower_components/jquery-form/jquery.form.js',
                     'templates/static/bower_components/qtip2/basic/jquery.qtip.min.js',
-                    'templates/static/bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'templates/static/lib/bootstrap/bootstrap.min.js',
                     'templates/static/bower_components/pickmeup/js/jquery.pickmeup.min.js',
                     'templates/static/bower_components/pickmeup/js/jquery.pickmeup.twitter-bootstrap.min.js',
                     'templates/static/lib/logger.js',
@@ -24,13 +25,20 @@ module.exports = function (grunt) {
                     'templates/static/lib/color.js',
                 ],
                 dest: 'templates/static/main.js'
+            }
+        },
+        cssmin: {
+            options: {
+                keepSpecialComments: 0
             },
+            main_style: {
+                files: [{
+                    src: 'templates/static/css/main.css',
+                    dest: 'templates/static/css/main.min.css'
+                }]
+            }
         },
         uglify: {
-            main_style: {
-                src: 'templates/static/main.css',
-                dest: 'templates/static/main.min.css'
-            },
             main_script: {
                 src: 'templates/static/main.js',
                 dest: 'templates/static/main.min.js'
@@ -40,10 +48,11 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask('deploy', 'deploying task', function () {
         grunt.task.run(['concat:main_script', 'uglify:main_script']);
-        grunt.task.run(['concat:marks_script', 'uglify:marks_script']);
-        grunt.task.run(['concat:main_style']);
+//        grunt.task.run(['concat:marks_script', 'uglify:marks_script']);
+        grunt.task.run(['concat:main_style', "cssmin:main_style"]);
     });
 };
