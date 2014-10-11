23aaea342d1eb6535ff40ed07148a3af48f91d1a
m <mmailm@mail.ru>
2014-10-11 20:24:50 +0900

    нмного поднастроил логирование, и webassets

--- app/settings_log.py
+++ app/settings_log.py
@@ -1,42 +1,43 @@
 import os
-from settings import BASE_DIR
+from settings import BASE_DIR, DEBUG
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        # Include the default Django email handler for errors
-        # This is what you'd get without configuring logging at all.
-        'mail_admins': {
-            'class': 'django.utils.log.AdminEmailHandler',
-            'level': 'ERROR',
-             # But the emails are plain text by default - HTML is nicer
-            'include_html': True,
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            # Include the default Django email handler for errors
+            # This is what you'd get without configuring logging at all.
+            'mail_admins': {
+                'class': 'django.utils.log.AdminEmailHandler',
+                'level': 'ERROR',
+                 # But the emails are plain text by default - HTML is nicer
+                'include_html': True,
+            },
+            # Log to a text file that can be rotated by logrotate
+            'logfile': {
+                'class': 'logging.handlers.WatchedFileHandler',
+                'filename': os.path.join(BASE_DIR, 'logs/log.log')
+            },
         },
-        # Log to a text file that can be rotated by logrotate
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/log.log')
+        'loggers': {
+            # Again, default Django configuration to email unhandled exceptions
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+            # Might as well log any errors anywhere else in Django
+            'django': {
+                'handlers': ['logfile'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+            # Your own app - this assumes all your logger names start with "myapp."
+            'app': {
+                'handlers': ['logfile'],
+                'level': 'WARNING', # Or maybe INFO or DEBUG
+                'propagate': False
+            },
         },
-    },
-    'loggers': {
-        # Again, default Django configuration to email unhandled exceptions
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        # Might as well log any errors anywhere else in Django
-        'django': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        # Your own app - this assumes all your logger names start with "myapp."
-        'app': {
-            'handlers': ['logfile'],
-            'level': 'WARNING', # Or maybe INFO or DEBUG
-            'propagate': False
-        },
-    },
-}
+    }
