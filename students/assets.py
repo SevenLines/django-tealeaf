96a77179cac4e7ae5bf91419784133b16964a351
m <mmailm@mail.ru>
2014-10-18 00:08:27 +0900

    оптимизировал темы и чуток улучшил вид синей темы

--- students/assets.py
+++ students/assets.py
@@ -6,9 +6,9 @@ get_env().append_path('templates/static')
 get_env().append_path("students/static")
 
 marks_js = Bundle('bower_components/knockout/dist/knockout.js',
-                 'js/ko.modal_confirm.js',
-                 'js/students/ko.marks.js',
-                 filters="yui_js",
-                 output="js/students/marks.min.js")
+                  'js/ko.modal_confirm.js',
+                  'js/students/ko.marks.js',
+                  filters="yui_js",
+                  output="js/students/marks.min.js")
 
 register("marks_js", marks_js)
