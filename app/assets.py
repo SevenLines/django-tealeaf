f11a059f94d779be11c0bcdd8b702a30dde05de6
m <mmailm@mail.ru>
2015-02-22 13:02:54 +0800

    убрал лишние скрипты

--- app/assets.py
+++ app/assets.py
@@ -3,18 +3,23 @@ from django_assets.env import get_env
 
 get_env().append_path("templates/static")
 
+# main_js = Bundle('bower_components/jquery/dist/jquery.min.js',
+#                  'bower_components/jquery.cookie/jquery.cookie.js',
+#                  'bower_components/jquery-form/jquery.form.js',
+#                  'bower_components/qtip2/basic/jquery.qtip.min.js',
+#                  'bower_components/pickmeup/js/jquery.pickmeup.min.js',
+#                  'bower_components/pickmeup/js/jquery.pickmeup.twitter-bootstrap.min.js',
+#                  'bower_components/underscore/underscore.js',
+                 # 'bower_components/Sortable/Sortable.min.js',
+                 # 'lib/bootstrap/bootstrap.min.js',
+                 # 'lib/logger.js',
+                 # 'js/interface.js',
+                 # 'lib/_color.js',
+                 # filters="yui_js",
+                 # output="js/main.min.js")
+
 main_js = Bundle('bower_components/jquery/dist/jquery.min.js',
-                 'bower_components/jquery.cookie/jquery.cookie.js',
-                 'bower_components/jquery-form/jquery.form.js',
-                 'bower_components/qtip2/basic/jquery.qtip.min.js',
-                 'bower_components/pickmeup/js/jquery.pickmeup.min.js',
-                 'bower_components/pickmeup/js/jquery.pickmeup.twitter-bootstrap.min.js',
-                 # 'bower_components/underscore/underscore.js',
-                 'bower_components/Sortable/Sortable.min.js',
-                 'lib/bootstrap/bootstrap.min.js',
-                 'lib/logger.js',
-                 'js/interface.js',
-                 'lib/_color.js',
+                 # 'bower_components/requirejs/require.js',
                  filters="yui_js",
                  output="js/main.min.js")
 
