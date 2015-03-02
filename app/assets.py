06e2e2f36e2f9550e3d0da8f5775cc3a19d16bd4
m <mmailm@mail.ru>
2015-03-02 19:37:57 +0800

    добавил gulp с минифкацией scss при деплое

--- app/assets.py
+++ app/assets.py
@@ -13,7 +13,20 @@ base_js = Bundle('bower_components/jquery/dist/jquery.js',
                  filters="yui_js",
                  output="js/dist/base.js")
 
-
+students_manager_js = Bundle("js/helpers.js",
+                             "bower_components/lodash/lodash.min.js",
+                             "bower_components/angular/angular.js",
+                             "bower_components/angular-route/angular-route.js",
+                             "bower_components/angular-animate/angular-animate.js",
+                             "js/students/manager/app.js",
+                             "js/students/manager/controllers/group.js",
+                             "js/students/manager/controllers/groups.js",
+                             "js/students/manager/controllers/years.js",
+                             "js/students/manager/models/_base.js",
+                             "js/students/manager/models/student.js",
+                             "js/students/manager/models/group.js",
+                             filters="jsmin",
+                             output="js/students/manager/dist/app.js")
 
 main_css = Bundle('lib/bootstrap/bootstrap.min.css',
                   'bower_components/qtip2/jquery.qtip.min.css',
@@ -23,4 +36,5 @@ main_css = Bundle('lib/bootstrap/bootstrap.min.css',
                   output="css/main.min.css")
 
 register("base-js", base_js)
+register("students_manager_js", students_manager_js)
 register("main_css", main_css)
