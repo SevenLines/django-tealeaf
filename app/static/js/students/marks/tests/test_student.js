d04b5cae8f05ea13792cf11195465f7a5739c168
m <mmailm@mail.ru>
2015-04-26 13:16:28 +0800

    настроил среду для тестирования javascript

--- app/static/js/students/marks/tests/test_student.js
+++ app/static/js/students/marks/tests/test_student.js
@@ -2,12 +2,23 @@
  * Created by m on 26.04.15.
  */
 define(['marks/student'], function (Student) {
-	student = new Student({
-		id: 1,
-		name:'Лилия',
-		second_name: 'Калина',
-		sum: 30,
-		marksTable: null,
-		marks: null,
+	describe("Student", function () {
+		var student;
+
+		beforeAll(function () {
+			student = new Student({
+				id: 1,
+				name: 'Лилия',
+				second_name: 'Калина',
+				sum: 30,
+				marksTable: null,
+				marks: null
+			});
+		});
+
+		it("full sum should return full sum", function () {
+			expect(student.full_sum).toEqual(30);
+		})
+
 	});
 });
